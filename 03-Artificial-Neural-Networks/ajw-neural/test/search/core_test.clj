46da89c5f648021444839c78b2cc488c69a8205e
aw5au <andrew.wood9@ntlworld.com>
2015-05-01 13:26:33 +0100

    Added functional versions of backpropagation.

--- 03-Artificial-Neural-Networks/ajw-neural/test/search/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns search.core-test
-  (:require [expectations :refer :all]
-            [search.core :refer :all]
-            [search.depth-first :refer [list-difference]]))
-
-
-;; An empty string should return 0
-(expect '[a s d] (list-difference '(a s d) '(f g h)))
-
