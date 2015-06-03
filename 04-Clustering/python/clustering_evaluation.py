ed704766776ba04397cbcae586e2da43a7dd166a
Dimitry Foures <dimitry.foures@gmail.com>
2015-06-03 20:08:24 +0100

    added 3d plot

--- 04-Clustering/python/clustering_evaluation.py
+++ 04-Clustering/python/clustering_evaluation.py
@@ -1,11 +1,12 @@
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 iris = load_iris()
 X = iris.data
 Y = iris.target
-
 
 clf = KMeans(n_clusters=3)
 clf.fit(X)
@@ -20,6 +21,14 @@ for i in [0,1,2]:
 mapping_predict_true = dict(zip(np.argmax(intersection,1),[0,1,2]))
 
 labels = map(lambda x: mapping_predict_true[x], labels)
+
+color_map = {False : 'r', True: 'g'}
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:,0],X[:,1],X[:,2], marker = 'o', color = map(lambda x: color_map[x], labels==Y))
+plt.show()
+
 
 print 'Accuracy: '
 print sum(labels==Y)/float(len(Y))
