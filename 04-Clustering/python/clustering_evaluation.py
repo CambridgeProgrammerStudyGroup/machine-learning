b9dfd23413790885a65e29841e8e0685fc28115f
James Briggs <jimy.pbr@gmail.com>
2015-06-03 19:24:55 +0100

    added kmeans

--- 04-Clustering/python/clustering_evaluation.py
+++ 04-Clustering/python/clustering_evaluation.py
@@ -1,7 +1,16 @@
 from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
 
 iris = load_iris()
 X = iris.data
 Y = iris.target
 
-print X, Y
+
+clf = KMeans(n_clusters=3)
+clf.fit(X)
+labels = clf.labels_
+
+labels = clf.predict(X)
+
+for label,target in zip(labels,Y):
+    print label,target
