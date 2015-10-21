19eea6e85ba07bfadf15b201cbf4966b6ee72558
Javi Merino <merino.jav@gmail.com>
2015-10-21 19:38:29 +0100

    Learn to classify text from the command line

--- 06-Naive-Bayes/spam_bayes.py
+++ 06-Naive-Bayes/spam_bayes.py
@@ -23,6 +23,33 @@ def classify_words(text, word_dict):
 
         word_dict[word] += 1
 
+def is_spam(text, ham_words, spam_words):
+    probability_ham = 0.5
+    probability_spam = 0.5
+
+    words = re.split(r"(?: |,|\.|-|–|:|;|&|=|\+|#|\(|\)|\||<|…|\^|\[|\])+", text)
+
+    for word in words:
+        # Remove misc symbols
+        word = re.sub(r"^(?:'|\"|“)+", r"", word)
+        word = re.sub(r"(\w)'$", r"\1", word)
+        word = word.lower()
+
+        if word == "":
+            continue
+
+        ham_instances = ham_words[word]
+        spam_instances = spam_words[word]
+        total_instances = ham_instances + spam_instances
+        if total_instances == 0:
+            continue
+
+        probability_ham *= ham_instances / total_instances
+        probability_spam *= spam_instances / total_instances
+        print("Spam: {} ham: {}".format(probability_spam, probability_ham))
+
+    return probability_spam > probability_ham
+
 def train(fname):
     ham_words = collections.defaultdict(int)
     spam_words = collections.defaultdict(int)
@@ -44,55 +71,44 @@ def test(fname):
     with open("brain", "rb") as fin:
         (ham_words, spam_words) = pickle.load(fin)
 
-    probability_ham = 0.5
-    probability_spam = 0.5
-
     with open(fname) as fin:
         for line in fin:
-            (true_clas, text) = line.split("\t")
+            (true_class, text) = line.split("\t")
 
-            words = re.split(r"(?: |,|\.|-|–|:|;|&|=|\+|#|\(|\)|\||<|…|\^|\[|\])+", text)
+            spam = is_spam(text, ham_words, spam_words)
 
-            for word in words:
-                # Remove misc symbols
-                word = re.sub(r"^(?:'|\"|“)+", r"", word)
-                word = re.sub(r"(\w)'$", r"\1", word)
-                word = word.lower()
+            guess = "spam" if spam else "ham"
 
-                if word == "":
-                    continue
-
-                ham_instances = ham_words[word]
-                spam_instances = spam_words[word]
-                total_instances = ham_instances + spam_instances
-                if total_instances == 0:
-                    continue
-
-                probability_ham *= ham_instances / total_instances
-                probability_spam *= spam_instances / total_instances
-                print("Spam: {} ham: {}".format(probability_spam, probability_ham))
-
-            if probability_spam > probability_ham:
-                guess = "spam"
-            else:
-                guess = "ham"
-
-            if (guess == true_clas):
+            if (spam and (true_class == "spam")) or \
+               (not spam and (true_class == "ham")):
                 correct = "Correct"
             else:
                 correct = "incorrect"
                 raise RuntimeError("Incorrect line: {}".format(line))
 
-            print("Our guess: {} (spam: {}, ham: {}), {}".format(guess, probability_spam, probability_ham, correct))
+            print("Our guess: {} {}".format(guess, correct))
+
+def classify_text(text):
+    with open("brain", "rb") as fin:
+        (ham_words, spam_words) = pickle.load(fin)
+
+    if is_spam(text, ham_words, spam_words):
+        print("spam")
+    else:
+        print("ham")
 
 def main():
     parser = argparse.ArgumentParser(description="Spam bayes classifier")
 
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--test", action="store_true")
+    parser.add_argument("-c", "--classify_text", nargs=1)
     parser.add_argument("file", nargs="?", default="corpus/SMSSpamCollection.txt", help="the file to read the text from.  Don't use the same file for training and testing ;)")
 
     args = parser.parse_args()
+
+    if args.classify_text:
+        return classify_text(args.classify_text[0])
 
     if args.train:
         train(args.file)
